b931c51ff75f96ddff74810548e047c396f6716c
danu44-1 <danudimas042@gmail.com>
2026-03-03 14:17:19 +0700

    Pengerjaan Pre-Test

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(
+                "===================================\nKARTU TANDA MAHASISWA \nNama\t:Danu Dimas Putra \nNIM\t:105224003 \nProdi\t:Ilmu Komputer \n=================================");
+    }
+}
